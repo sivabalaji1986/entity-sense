b8a4b433001f3f0b75d320052553ebeef9b1e5f6
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-07-29 18:44:09 +0800

    Created a Dummy Constant file

---
+++ src/main/java/com/hbs/entitysense/constants/EntitySenseConstant.java
@@ -0,0 +1,37 @@
+package com.hbs.entitysense.constants;
+
+public class EntitySenseConstant {
+
+    public static final String HIGH_RISK_LABEL       = "high-risk";
+
+    public static final String LOW_RISK_LABEL        = "low-risk";
+
+    public static final String HIGH_VALUE_LABEL      = "high-value";
+
+    public static final String MEDIUM_VALUE_LABEL    = "medium";
+
+    public static final String LOW_VALUE_LABEL       = "low";
+
+    public static final String NEWLY_OPENED_LABEL    = "newly opened";
+
+    public static final String ESTABLISHED_LABEL     = "established";
+
+    public static final String OLLAMA_URL       = "http://localhost:11434";
+
+    public static final String OLLAMA_EMBEDDINGS_URL       = "/api/embeddings";
+
+    public static final String OLLAMA_EMBEDDINGS_REQ_MODEL_KEY      = "model";
+
+    public static final String OLLAMA_EMBEDDINGS_REQ_MODEL_VALUE      = "nomic-embed-text";
+
+    public static final String OLLAMA_EMBEDDINGS_REQ_PROMPT_KEY      = "prompt";
+
+    public static final String OLLAMA_EMBEDDINGS_RES_EMBEDDING_KEY      = "embedding";
+
+    public static final String OPEN_API_SPEC_TITLE       = "MuleTrap API";
+
+    public static final String OPEN_API_SPEC_DESCRIPTION      = "Spring Boot API for Mule Account Detection using Vector Embeddings";
+
+    public static final String OPEN_API_SPEC_VERSION      = "1.0.0";
+
+}
