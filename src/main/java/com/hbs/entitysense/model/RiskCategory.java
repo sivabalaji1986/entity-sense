1acce30421d7bdefb38783a67b2f38f5d30edf10
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-07-29 18:46:19 +0800

    Created RiskCategory Enum

---
+++ src/main/java/com/hbs/entitysense/model/RiskCategory.java
@@ -0,0 +1,11 @@
+package com.hbs.entitysense.model;
+
+public enum RiskCategory {
+    SANCTION,
+    MULE,
+    MONEY_LAUNDERING,
+    CYBER_THREAT,
+    SHELL_ENTITY,
+    PEP,
+    SCAM_ENTITY
+}
