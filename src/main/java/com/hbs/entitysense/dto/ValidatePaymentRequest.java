3e272b00dcf64bef58c7983e6dffb482d6fb4f07
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-07-29 19:48:33 +0800

    Added constructors for ValidatePaymentRequest

--- src/main/java/com/hbs/entitysense/dto/ValidatePaymentRequest.java
+++ src/main/java/com/hbs/entitysense/dto/ValidatePaymentRequest.java
@@ -1,9 +1,13 @@
 package com.hbs.entitysense.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ValidatePaymentRequest {
     @NotBlank private String payeeName;
     private String payeeAddress;
