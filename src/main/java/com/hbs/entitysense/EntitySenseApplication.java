93b1366cfaa6f4222daf78cce050cf6ad3c3305e
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-07-29 18:47:30 +0800

    Created Main Application for EntitySense

---
+++ src/main/java/com/hbs/entitysense/EntitySenseApplication.java
@@ -0,0 +1,11 @@
+package com.hbs.entitysense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntitySenseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EntitySenseApplication.class, args);
+    }
+}
