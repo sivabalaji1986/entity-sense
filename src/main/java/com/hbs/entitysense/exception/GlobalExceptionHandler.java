7e38c445025598bd6b28e268f9d129f91f1b44f5
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-07-29 18:46:07 +0800

    Created Global Exception handler

---
+++ src/main/java/com/hbs/entitysense/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.hbs.entitysense.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntime(RuntimeException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
