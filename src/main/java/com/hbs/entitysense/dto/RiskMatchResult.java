e190f141aa8e4a3b9cf31b3b8dc52a8143919806
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-07-29 18:45:56 +0800

    Created DTO for RiskMatchResult

---
+++ src/main/java/com/hbs/entitysense/dto/RiskMatchResult.java
@@ -0,0 +1,13 @@
+package com.hbs.entitysense.dto;
+
+import com.hbs.entitysense.model.RiskCategory;
+import lombok.Data;
+
+@Data
+public class RiskMatchResult {
+    private Long id;
+    private String name;
+    private RiskCategory riskCategory;
+    private boolean matchedAccount;
+    private double distance;
+}
