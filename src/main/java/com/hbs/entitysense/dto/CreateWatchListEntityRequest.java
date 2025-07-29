0ee4f24a26cc9d62acba1e7f9b9d08e091b4983d
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-07-29 18:44:49 +0800

    Created CreateWatchListEntityRequest DTO

---
+++ src/main/java/com/hbs/entitysense/dto/CreateWatchListEntityRequest.java
@@ -0,0 +1,16 @@
+package com.hbs.entitysense.dto;
+
+import com.hbs.entitysense.model.RiskCategory;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CreateWatchListEntityRequest {
+    @NotBlank private String name;
+    private String address;
+    private String country;
+    private List<String> knownAccounts;
+    private RiskCategory riskCategory;
+}
